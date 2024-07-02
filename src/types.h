702e7555b7b69a96833295b15a7478671254e47c
Chase Barman <chasedbarman@gmail.com>
2024-07-01 17:03:37 -0700

    Updated color scheme

--- src/types.h
+++ src/types.h
@@ -3,13 +3,13 @@
 #include <SDL2/SDL.h>
 #include <stdlib.h>
 
-static SDL_Color COLOR_BG = { 0, 0, 0, 255 };
+static SDL_Color COLOR_BG = { 19, 32, 69, 255 };
 static SDL_Color COLOR_BLACK = { 0, 0, 0, 255 };
 static SDL_Color COLOR_WHITE = { 255, 255, 255, 255 };
-static SDL_Color COLOR_LIGHT_BLUE = { 173, 216, 230, 255 };
-static SDL_Color COLOR_DARK_BLUE = { 0, 0, 139, 255 };
-static SDL_Color COLOR_ORANGE = { 255, 165, 0, 255 };
-static SDL_Color COLOR_YELLOW = { 255, 255, 0, 255 };
-static SDL_Color COLOR_GREEN = { 0, 128, 0, 255 };
-static SDL_Color COLOR_PURPLE = { 128, 0, 128, 255 };
-static SDL_Color COLOR_RED = { 255, 0, 0, 255 };
+static SDL_Color COLOR_LIGHT_BLUE = { 64, 245, 255, 255 };
+static SDL_Color COLOR_DARK_BLUE = { 45, 93, 255, 255 };
+static SDL_Color COLOR_ORANGE = { 255, 208, 42, 255 };
+static SDL_Color COLOR_YELLOW = { 241, 234, 32, 255 };
+static SDL_Color COLOR_GREEN = { 79, 255, 64, 255 };
+static SDL_Color COLOR_PURPLE = { 255, 56, 255, 255 };
+static SDL_Color COLOR_RED = { 255, 58, 42, 255 };
